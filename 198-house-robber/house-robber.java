cfffe24d33ce227ef50404eba5c0145d68883bc7
Prateek Jain <jainprateek2004@gmail.com>
2025-07-22 18:03:22 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (14.95%) - LeetSync

---
+++ 198-house-robber/house-robber.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int rob(int[] nums) {
+        if (nums.length == 0) return 0;
+        if (nums.length == 1) return nums[0];
+
+        int prev1 = 0; // max rob till house i-1
+        int prev2 = 0; // max rob till house i-2
+
+        for (int num : nums) {
+            int current = Math.max(prev1, prev2 + num);
+            prev2 = prev1;
+            prev1 = current;
+        }
+
+        return prev1;
+    }
+}
